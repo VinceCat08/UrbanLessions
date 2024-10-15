bd84013d54b049a3823a8b92cba25260e12b1609
VinceCat08 <bandcatworld@gmail.com>
2024-10-15 18:10:55 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,17 @@
+print('1st program')
+print('Ответ:', 9 ** 0.5 * 5)
+
+print('2nd program')
+print('Ответ:', 9.99 > 9.98 and 1000 != 1000.1)
+
+print('3rd program')
+a=2 * 2 + 2
+b=2 * (2+2)
+print('Первый ответ:', a)
+print('Второй ответ:', b)
+print('Сравнение первого и второго ответа:', a == b)
+
+
+print('4th program')
+print('Ответ:', int(123.456 * 10) % 10)
+
